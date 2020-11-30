654d222089a35ca4846500171b2f2d0fa426f453
Ralph' Potato <fpilot.github@gmail.com>
2020-11-30 01:46:50 +0100

    Add correct colors, breakpoints and spacings

--- packages/fig-theming/src/theme.ts
+++ packages/fig-theming/src/theme.ts
@@ -1,53 +1,79 @@
-import { Theme, ThemeColors } from '$types'
+import { DragonColors, Theme, ThemeBreakpoints, ThemeColors, ThemeSpacings } from '$types'
+import { SizeUnit } from '@figouzes/falcon-css'
+
+export const dragonColors: DragonColors = {
+  red: {
+    c100: [97, 35, 43],
+    c300: [97, 0, 13],
+    c500: [173, 0, 23],
+    c700: [224, 0, 30],
+    c900: [250, 55, 81],
+  },
+  blue: {
+    c100: [41, 52, 92],
+    c300: [13, 29, 87],
+    c500: [28, 62, 186],
+    c700: [32, 72, 214],
+    c900: [139, 161, 240],
+  },
+  green: {
+    c100: [69, 92, 78],
+    c300: [39, 87, 58],
+    c500: [84, 186, 125],
+    c700: [96, 214, 144],
+    c900: [50, 240, 126],
+  },
+  gold: {
+    c100: [122, 80, 2],
+    c300: [173, 114, 3],
+    c500: [187, 142, 60],
+    c700: [250, 165, 4],
+    c900: [250, 190, 80],
+  },
+  white: {
+    c100: [119, 115, 112],
+    c300: [166, 160, 156],
+    c500: [181, 181, 181],
+    c700: [240, 219, 204],
+    c900: [242, 234, 228],
+  },
+  black: {
+    c100: [0, 0, 0],
+    c300: [0, 9, 10],
+    c500: [0, 15, 18],
+    c700: [0, 37, 43],
+    c900: [0, 46, 54],
+  },
+}
 
 export const colors: ThemeColors = {
-  dragons: {
-    red: {
-      c100: [95, 16, 16],
-      c300: [150, 27, 27],
-      c500: [210, 40, 40],
-      c700: [240, 92, 92],
-      c900: [255, 170, 170],
-    },
-    blue: {
-      c100: [16, 87, 94],
-      c300: [27, 129, 140],
-      c500: [40, 193, 210],
-      c700: [91, 225, 240],
-      c900: [171, 247, 255],
-    },
-    green: {
-      c100: [16, 94, 49],
-      c300: [27, 140, 74],
-      c500: [40, 209, 110],
-      c700: [91, 240, 153],
-      c900: [171, 255, 206],
-    },
-    gold: {
-      c100: [94, 84, 16],
-      c300: [140, 125, 27],
-      c500: [209, 187, 40],
-      c700: [240, 220, 91],
-      c900: [255, 244, 171],
-    },
-    white: {
-      c100: [99, 99, 99],
-      c300: [145, 145, 145],
-      c500: [196, 196, 196],
-      c700: [217, 217, 217],
-      c900: [247, 247, 247],
-    },
-    black: {
-      c100: [26, 26, 26],
-      c300: [46, 46, 46],
-      c500: [61, 61, 61],
-      c700: [102, 102, 102],
-      c900: [143, 143, 143],
-    },
-  },
+  dragons: dragonColors,
+  info: dragonColors.blue,
+  success: dragonColors.green,
+  error: dragonColors.red,
+}
+
+// Spacings
+export const spacings: ThemeSpacings = {
+  tiny: [4, SizeUnit.Px],
+  small: [8, SizeUnit.Px],
+  medium: [16, SizeUnit.Px],
+  large: [24, SizeUnit.Px],
+  xl: [32, SizeUnit.Px],
+  xxl: [48, SizeUnit.Px],
+  xxxl: [64, SizeUnit.Px],
+}
+
+// Breakpoints
+export const breakpoints: ThemeBreakpoints = {
+  mobile: [768, SizeUnit.Px],
+  tablet: [1024, SizeUnit.Px],
+  desktop: [1440, SizeUnit.Px],
+  widescreen: [1824, SizeUnit.Px],
 }
 
 // Compose theme
 export const theme: Theme = {
   colors,
+  spacings,
 }
--- packages/fig-theming/src/types/theme.ts
+++ packages/fig-theming/src/types/theme.ts
@@ -1,16 +1,43 @@
-import { ColorPalette } from '@figouzes/falcon-css'
+import { ColorPalette, CssSize } from '@figouzes/falcon-css'
+
+export interface DragonColors {
+  red: ColorPalette
+  blue: ColorPalette
+  black: ColorPalette
+  white: ColorPalette
+  green: ColorPalette
+  gold: ColorPalette
+}
 
 export interface ThemeColors {
-  dragons: {
-    red: ColorPalette
-    blue: ColorPalette
-    black: ColorPalette
-    white: ColorPalette
-    green: ColorPalette
-    gold: ColorPalette
-  }
+  dragons: DragonColors
+  info: ColorPalette
+  success: ColorPalette
+  error: ColorPalette
+}
+
+export type ThemeColor = keyof ThemeColors
+
+export interface ThemeSpacings {
+  tiny: CssSize
+  small: CssSize
+  medium: CssSize
+  large: CssSize
+  xl: CssSize
+  xxl: CssSize
+  xxxl: CssSize
+}
+
+export type ThemeSpacing = keyof ThemeSpacings
+
+export interface ThemeBreakpoints {
+  mobile: CssSize
+  tablet: CssSize
+  desktop: CssSize
+  widescreen: CssSize
 }
 
 export interface Theme {
   colors: ThemeColors
+  spacings: ThemeSpacings
 }
