0af8d3e08ddbf2674de22b1b63b465d447573e0c
Ralph' Potato <fpilot.github@gmail.com>
2020-11-30 02:20:22 +0100

    Add font-style util

--- packages/falcon-css/src/utils/styling/text.ts
+++ packages/falcon-css/src/utils/styling/text.ts
@@ -1,3 +1,5 @@
+import * as Css from 'csstype'
+
 import { CssSize, Hue } from '$types'
 
 import { cssProperty } from './properties'
@@ -6,6 +8,8 @@ import { cssSize, hue, stringValue } from './parsers'
 export const fontFamily = cssProperty<string>(stringValue)('font-family')
 
 export const fontSize = cssProperty<CssSize>(cssSize)('font-size')
+
+export const fontStyle = cssProperty<Css.Property.FontStyle>(stringValue)('font-style')
 
 export const fontWeight = cssProperty<CssSize>(cssSize)('font-weight')
 
