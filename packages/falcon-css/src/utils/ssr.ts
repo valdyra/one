31a43e5081e136a3e6b84f67028ef7a19a058341
Ralph' Potato <fpilot.github@gmail.com>
2020-11-30 02:16:05 +0100

    Add SSR util for font-face

--- packages/falcon-css/src/utils/ssr.ts
+++ packages/falcon-css/src/utils/ssr.ts
@@ -1,2 +1,6 @@
 export const interpolateProperties = (selectors: string[]) => (properties: string) =>
   `${selectors.join(',')} {${properties}}`
+
+export const interpolateProperty = (selector: string) => interpolateProperties([selector])
+
+export const fontFace = interpolateProperty('@font-face')
